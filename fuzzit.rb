3e9c59038b287191418a7c0f25c7f422b57d4e9c
goreleaserbot <goreleaser@carlosbecker.com>
2019-09-09 00:36:07 +0300

    Brew formula update for fuzzit version v2.4.42

--- fuzzit.rb
+++ fuzzit.rb
@@ -2,16 +2,16 @@
 class Fuzzit < Formula
   desc "Continuous Fuzzing Made Simple."
   homepage "https://fuzzit.dev"
-  version "2.4.41"
+  version "2.4.42"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.41/fuzzit_Darwin_x86_64.zip"
-    sha256 "20839c06cbb763650c69fb5f04a31c3bf7bd0b9e5713d8743929b25d4f73add7"
+    url "https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.42/fuzzit_Darwin_x86_64.zip"
+    sha256 "ddac20dcd95eaeafdf7d6e69e1fd3e8b3fc41f167c069d3349c284d53ff44caf"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.41/fuzzit_Linux_x86_64.zip"
-      sha256 "42cc4d58474cd1a5239bf259beaf39e1c6a5e2315a1412bd15da1a4f5967b5f6"
+      url "https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.42/fuzzit_Linux_x86_64.zip"
+      sha256 "02bc2b7750ecf330deab23ad9ec3d315ee691121c130a21bfe83582d74e9f699"
     end
   end
 
